b753b4dd015de985abd49a59eace935e51e8e825
Soulike <316413310@qq.com>
2018-11-13 21:45:10 +0800

    修复bug

--- routers/NoteManagement.py
+++ routers/NoteManagement.py
@@ -1,10 +1,11 @@
+from datetime import datetime
+
 from flask import Blueprint, request
 
+import database
+from functions import session, note, log
 from modules import MDConverter
 from objects import Response
-from datetime import datetime
-from functions import session, note, log
-import database
 
 NoteManagement = Blueprint('NoteManagement', __name__)
 
@@ -28,7 +29,7 @@ def submitNote():
             fileName = req['fileName']
             noteContent = req['noteContent']
             noteId = req['noteId']
-            nowTime = datetime.utcnow()
+            nowTime = datetime.now()
 
             if fileName is None or noteContent is None or noteId is None:
                 res = Response(False, '提交格式非法', {})
@@ -62,8 +63,8 @@ def submitNote():
                 else:
                     # 这一步操作失败了也没有太大关系
                     cur.execute(
-                        'UPDATE {0} SET {1} = {2} WHERE {3}=%s'.format('notes', 'last_update_time', nowTime, 'noteId'),
-                        noteId)
+                        'UPDATE {0} SET {1}=%s WHERE {2}=%s'.format('notes', 'last_update_time', 'id'),
+                        (nowTime, noteId))
                     conn.commit()
                     res = Response(True, '修改成功', {})
         cur.close()
@@ -72,20 +73,9 @@ def submitNote():
 
 @NoteManagement.route('/deleteNote', methods = ['POST'])
 def deleteNote():
-    pass
-
-
-@NoteManagement.route('/getNoteList', methods = ['GET'])
-def getNoteList():
-    res = Response(True, '笔记列表获取成功', [])
-    return res.getJson()
-
-
-@NoteManagement.route('/getNote', methods = ['GET'])
-def getNote():
     userId = session.getSessionUserId()
     if userId is None:
-        res = Response(False, '登录状态失效', {}).getJson()
+        res = Response(False, '登录状态失效', {})
     else:
         conn = database.conn
         cur = conn.cursor()
@@ -97,15 +87,85 @@ def getNote():
         else:
             req = request.get_json()
 
-            noteId = req['id']
+            noteId = req.get('id')
 
-            if noteId is None or noteId <= 0:
+            if noteId is None or int(noteId) <= 0:
+                res = Response(False, '提交格式非法', {})
+            else:
+                try:
+                    cur.execute('DELETE FROM {0} WHERE {1}=%s'.format('notes', 'id'), (noteId,))
+                    conn.commit()
+                except IOError as e:
+                    conn.rollback()
+                    res = Response(False, '删除失败', {})
+                    log(e)
+                else:
+                    if note.deleteNoteFile(noteId) is True:
+                        res = Response(True, '删除成功', {})
+                    else:
+                        res = Response(True, '删除失败', {})
+        cur.close()
+    return res.getJson()
+
+
+@NoteManagement.route('/getNoteList', methods = ['GET'])
+def getNoteList():
+    userId = session.getSessionUserId()
+    if userId is None:
+        res = Response(False, '登录状态失效', {})
+    else:
+        conn = database.conn
+        cur = conn.cursor()
+        cur.execute('SELECT {0} FROM {1} WHERE {2}=%s'.format('username', 'accounts', 'id'), (userId,))
+        result = cur.fetchone()
+
+        if result is None or len(result) == 0:
+            res = Response(False, '用户不存在', {})
+        else:
+            try:
+                cur.execute('SELECT {0},{1},{2} FROM {3}'.format('id', 'name', 'last_update_time', 'notes'))
+            except IOError as e:
+                res = Response(False, '列表获取失败', {})
+                log(e)
+            else:
+                results = cur.fetchall()
+                data = []
+                for result in results:
+                    data.append({
+                        'id': result[0],
+                        'name': result[1],
+                        'lastModified': str(result[2])
+                    })
+                res = Response(True, '获取成功', data)
+        cur.close()
+    return res.getJson()
+
+
+@NoteManagement.route('/getNote', methods = ['GET'])
+def getNote():
+    userId = session.getSessionUserId()
+    if userId is None:
+        res = Response(False, '登录状态失效', {})
+    else:
+        conn = database.conn
+        cur = conn.cursor()
+        cur.execute('SELECT {0} FROM {1} WHERE {2}=%s'.format('username', 'accounts', 'id'), (userId,))
+        result = cur.fetchone()
+
+        if result is None or len(result) == 0:
+            res = Response(False, '用户不存在', {})
+        else:
+            req = request.args
+
+            noteId = req.get('id')
+
+            if noteId is None or int(noteId) <= 0:
                 res = Response(False, '提交格式非法', {})
             else:
                 try:
                     noteContent = note.readNoteFile(noteId)
                     cur.execute(
-                        'SELECT {0},{1} FROM {2} WHERE {3}=%s'.format('name', 'last_modify_time', 'notes', 'id'),
+                        'SELECT {0},{1} FROM {2} WHERE {3}=%s'.format('name', 'last_update_time', 'notes', 'id'),
                         noteId)
                     result = cur.fetchone()
                     title = result[0]
@@ -115,10 +175,48 @@ def getNote():
                     log(e)
                 else:
                     converter = MDConverter()
-                    res = Response(True, '获取成功', {'title': title, 'lastModifiedDate': last_modify_time,
+                    res = Response(True, '获取成功', {'title': title, 'lastModifiedDate': str(last_modify_time),
                                                   'content': converter.makeHtml(noteContent)})
         cur.close()
+    return res.getJson()
 
+
+@NoteManagement.route('/getNoteRaw', methods = ['GET'])
+def getNoteRaw():
+    userId = session.getSessionUserId()
+    if userId is None:
+        res = Response(False, '登录状态失效', {})
+    else:
+        conn = database.conn
+        cur = conn.cursor()
+        cur.execute('SELECT {0} FROM {1} WHERE {2}=%s'.format('username', 'accounts', 'id'), (userId,))
+        result = cur.fetchone()
+
+        if result is None or len(result) == 0:
+            res = Response(False, '用户不存在', {})
+        else:
+            req = request.args
+
+            noteId = req.get('id')
+
+            if noteId is None or int(noteId) <= 0:
+                res = Response(False, '提交格式非法', {})
+            else:
+                try:
+                    noteContent = note.readNoteFile(noteId)
+                    cur.execute(
+                        'SELECT {0},{1} FROM {2} WHERE {3}=%s'.format('name', 'last_update_time', 'notes', 'id'),
+                        noteId)
+                    result = cur.fetchone()
+                    title = result[0]
+                except IOError as e:
+                    res = Response(False, '服务器错误', {})
+                    log(e)
+                else:
+                    res = Response(True, '获取成功', {'title': title,
+                                                  'content': noteContent})
+        cur.close()
+    print(res.getJson())
     return res.getJson()
 
 
